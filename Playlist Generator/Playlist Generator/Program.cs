d402de5b095c1afb3f1c06d4070fdb5c42ee4448
alyssammayo <alyssammayo@outlook.com>
2025-01-03 14:54:04 -0600

    Add files via upload
    Added the initial files. Added some notes to Program.cs

---
+++ Playlist Generator/Playlist Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿// Playlist Genrtator
+
+using System;
+
+
+Console.WriteLine("Playlist Generator");
+
+
+
+
+/*
+  Notes:
+ 
+ */
+
+
+/*
+  Maybe include:
+
+For creating a menu.
+enum Menu 
+{
+
+    New Playlist,
+    Load Playlist,
+    Edit Playlist,
+    Exit
+
+}
+ 
+ */
